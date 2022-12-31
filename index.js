5f0076e68cbbf113cad52c5a9832b4b24e72635a
LautaroE3 <lnespinoza@escuelasproa.edu.ar>
2022-12-31 15:17:58 -0300

    Tercer commit-Configurando todo

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
-
+const path = require("path");
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
 app.use("/", home);
+app.set("view engine", "ejs");
+//Defino la localización de mis vistas
+app.set("views", path.join(__dirname, "views"));
 
 // connection
 const port = process.env.PORT || 3005;
