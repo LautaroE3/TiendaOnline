b85a5c8dfe90185537cdab0837bbb0785065c169
LautaroE3 <lnespinoza@escuelasproa.edu.ar>
2023-01-05 00:49:06 -0300

    Avances con l,a base de datos

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
+//Base de Datos
+const mongoose = require("mongoose");
+
 // Routes
 app.use("/", home);
 app.set("view engine", "ejs");
@@ -16,3 +19,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 // connection
 const port = process.env.PORT || 3005;
 app.listen(port, () => console.log(`Listening to port ${port}`));
+
+const DbCredentials="mongodb+srv://DuTienda:ne3sDSoBv3rbGs6a@tiendaonline.hwuvnq5.mongodb.net/test";
+//process.env.DB_KEY
+mongoose.set("strictQuery", false);
+mongoose.connect(DbCredentials, {
+    serverSelectionTimeoutMS:0, 
+}) 
+.then((con) => {
+    console.log("Conectado a la DB");
+});
